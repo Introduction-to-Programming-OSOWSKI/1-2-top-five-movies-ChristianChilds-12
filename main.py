227632003d2f82e2648e356e530ab1489ddc98f3
ChristianChilds-12 <821218@mystma.org>
2022-09-16 14:38:59 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-#WRITE YOUR CODE IN THIS FILE
+def topFiveMovies(a, b, c, d, e):
+    return a + " " + b + " " + c + " " + d + " " + e
+print(topFiveMovies("Marvel", "Starwars", "Despicable Me", "Transformers", "The Hobbit"))
+    
+
+
